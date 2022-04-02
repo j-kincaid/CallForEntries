673ee348bf5bc8072bfb70b273e3369af443654f
Jessica Kincaid <4j.kincaid@gmail.com>
2022-04-02 12:05:48 -0500

    Update views to display panelists

--- panel/urls.py
+++ panel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('panel', views.panel),
-    path('authorized', views.authorized),
+    path('panel', views.PanelView.as_view()),
+    path('authorized', views.AuthorizedView.as_view()),
+    path('panel/<int:pk>', views.PanelListView.as_view()),
 ]
--- panel/views.py
+++ panel/views.py
@@ -1,10 +1,31 @@
+from django.http import Http404
 from django.shortcuts import render
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Panel
+# class-based views
+from django.views.generic import TemplateView, ListView, DetailView
 
-def panel(request):
-    return render(request, 'panel/welcome.html', {'today': datetime.today})
+class PanelDetailView(DetailView):
+    model = Panel
+    context_object_name = 'panel'
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'panel/authorized.html', {})
+class PanelListView(ListView):
+    model = Panel
+    context_object_name = 'panels'
+    template_name = 'panel/panel_list.html'
+
+class PanelView(TemplateView):
+    template_name = 'panel/welcome.html'
+    extra_content = {'today': datetime.today()}
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'panel/authorized.html'
+    login_url = '/admin'
+
+def detail(request, pk):
+    try:
+        panel = Panel.objects.get(pk=pk)
+    except Panel.DoesNotExist:
+        raise Http404("Panelist doesn't exist")
+    return render(request, 'panel/panel_list.html', {'panel': panel})
