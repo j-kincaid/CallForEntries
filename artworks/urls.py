b3b2afc92ae72aea1d21133a798d6ed2a256a652
Jessica Kincaid <4j.kincaid@gmail.com>
2023-01-13 15:44:50 -0600

    add urls and views to artworks, templates for root directory

---
+++ artworks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('artworks/', views.artworks, name="artworks"),
+    path('artwork/<str:pk>/', views.artwork, name="artwork"),
+]
--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def artworks(request):
+    return HttpResponse('Here are the artworks')
+
+
+def artwork(request, pk):
+    return HttpResponse('Single artwork' + ' ' + str(pk))
+
