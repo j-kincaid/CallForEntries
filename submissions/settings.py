08cb4693dd521df2dd5bc457de9a1a8cadfdd26a
Jessica Kincaid <4j.kincaid@gmail.com>
2023-01-13 11:17:14 -0600

    create artworks app and register it in settings

--- submissions/settings.py
+++ submissions/settings.py
@@ -37,10 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # apps
-    'panel',
-    'artists',
+    'artists.apps.ArtistsConfig',
+    'artworks.apps.ArtworksConfig',
+    'panel.apps.PanelConfig',
 ]
 
 MIDDLEWARE = [
